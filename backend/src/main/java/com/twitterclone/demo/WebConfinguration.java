c59fd2e836247503186854ae2fc4d5472165ac1d
patni1992 <patriknilsson1992@gmail.com>
2020-10-11 14:01:47 +0200

    enable cors

---
+++ backend/src/main/java/com/twitterclone/demo/WebConfinguration.java
@@ -0,0 +1,13 @@
+package com.twitterclone.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
