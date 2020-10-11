bbc5b4cf9e927acab77a2813e3e837a9625ace02
patni1992 <patriknilsson1992@gmail.com>
2020-10-11 14:02:56 +0200

    fetch posts from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import {
+  Button,
+  Container,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Card,
+  CardContent,
+  CardHeader,
+  Avatar,
+  Fab,
+  Box,
+  makeStyles,
+  Grid,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import AddIcon from "@material-ui/icons/Add";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const fabStyle = {
+  right: 30,
+  bottom: 30,
+  position: "fixed",
+};
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = () => {
+    return fetch("http://localhost:8080/posts").then((resp) => resp.json());
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const posts = await fetchPosts();
+      setPosts(posts);
+    };
+
+    fetchData();
+  }, []);
+
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Twitter clone
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Box m={10}>
+          <Grid container spacing={3}>
+            {posts.map((post) => (
+              <Grid item xs={4} key={post.id}>
+                <Card variant="outlined" style={{ textAlign: "left" }}>
+                  <CardHeader
+                    avatar={
+                      <Avatar aria-label="recipe" className={classes.avatar}>
+                        R
+                      </Avatar>
+                    }
+                    title="Patrik"
+                    subheader="September 14, 2016"
+                  />
+                  <CardContent>
+                    <Typography variant="body2" component="p">
+                      {post.message}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
+      <Fab style={fabStyle} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
     </div>
   );
 }
